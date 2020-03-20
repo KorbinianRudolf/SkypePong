0af59c6f5d29f7df1f61d63e1f8ec72eb68f118c
Korbinian <korbinian.rudolf@outlook.de>
2020-03-20 17:21:37 +0100

    spelling

--- createGameEnvironment.js
+++ createGameEnvironment.js
@@ -9,7 +9,7 @@ var intervalHolder = null;
 var intervalHolder2 = null;
 var url = window.location.protocol + '//' + window.location.host + '/SkypePong/status.json';
 console.log(url);
-var fieldWith = gameCanvas.width/3;
+var fieldWidth = gameCanvas.width/3;
 
 
 function updateDisplay() {
@@ -54,16 +54,16 @@ gameCanvas.addEventListener('click', function(event) {
    var start = -1;
    var cl = [];
 
-   if(x < fieldWith) {
+   if(x < fieldWidth) {
        start = 0;
        cl = clicked1;
        console.log("1");
-   } else if (x < fieldWith*2) {
-       start = fieldWith;
+   } else if (x < fieldWidth*2) {
+       start = fieldWidth;
        cl = clicked2;
        console.log("2")
-   } else if (x < fieldWith*3) {
-       start = fieldWith*2;
+   } else if (x < fieldWidth*3) {
+       start = fieldWidth*2;
        cl = clicked3;
        console.log("3");
    } else {
@@ -72,19 +72,19 @@ gameCanvas.addEventListener('click', function(event) {
    }
 
 
-   var dis =(fieldWith/4);
+   var dis =(fieldWidth/4);
    var eps = CONSTANTS.CUP_RADIUS;
-   if((Math.abs((start + (fieldWith/4)) - x) <= eps ) && (Math.abs(15 - y) <= eps)){
+   if((Math.abs((start + (fieldWidth/4)) - x) <= eps ) && (Math.abs(15 - y) <= eps)){
         cl[0] = cl[0] === 0? 1 : 0;
-   } else if((Math.abs((start + (fieldWith/2)) - x) <= eps) && (Math.abs(15 - y) <= eps)) {
+   } else if((Math.abs((start + (fieldWidth/2)) - x) <= eps) && (Math.abs(15 - y) <= eps)) {
        cl[1] = cl[1] === 0? 1 : 0;
-   } else if((Math.abs((start + ((fieldWith/4)*3)) - x) <= eps) && (Math.abs(15 - y) <= eps)) {
+   } else if((Math.abs((start + ((fieldWidth/4)*3)) - x) <= eps) && (Math.abs(15 - y) <= eps)) {
        cl[2] = cl[2] === 0? 1 : 0;
-   } else if((Math.abs((start + (fieldWith/3)) - x ) <= eps) && (Math.abs((15+dis) - y) <= eps)) {
+   } else if((Math.abs((start + (fieldWidth/3)) - x ) <= eps) && (Math.abs((15+dis) - y) <= eps)) {
        cl[3] = cl[3] === 0? 1 : 0;
-   } else if((Math.abs((start + (fieldWith/3)*2) - x ) <= eps) && (Math.abs((15+dis) - y) <= eps)) {
+   } else if((Math.abs((start + (fieldWidth/3)*2) - x ) <= eps) && (Math.abs((15+dis) - y) <= eps)) {
        cl[4] = cl[4] === 0? 1 : 0;
-   } else if((Math.abs((start + (fieldWith/2)) - x ) <= eps) && (Math.abs((15+2*dis) - y) <= eps)) {
+   } else if((Math.abs((start + (fieldWidth/2)) - x ) <= eps) && (Math.abs((15+2*dis) - y) <= eps)) {
        cl[5] = cl[5] === 0? 1 : 0;
    }
 
@@ -146,9 +146,9 @@ function mainLoop() {
     var context = gameCanvas.getContext('2d');
     context.clearRect(0,0,gameCanvas.width, gameCanvas.height);
 
-    drawGameField(context, 0, fieldWith, clicked1, "Roman");
-    drawGameField(context, fieldWith, fieldWith, clicked2, "Rinz");
-    drawGameField(context, fieldWith*2, fieldWith, clicked3, "Korbinian");
+    drawGameField(context, 0, fieldWidth, clicked1, "Roman");
+    drawGameField(context, fieldWidth, fieldWidth, clicked2, "Rinz");
+    drawGameField(context, fieldWidth*2, fieldWidth, clicked3, "Korbinian");
 
 
 }
